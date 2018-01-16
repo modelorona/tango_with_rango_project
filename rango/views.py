08a0be1cb855efeadd60dbeb848b74f978828fe7
Anguel Hristozov <2255541h@student.gla.ac.uk>
2018-01-16 15:42:54 +0000

    added files from uni

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Rango says hi there partner")
+    html = "<html><head></head><body>Hi there. <a href='/rango/about/'>About page</a></body></html>"
+    return HttpResponse(html)
+
+
+def about(request):
+    html = "<html><head></head><body>Hi there. <a href='/rango/'>Go back to home page.</a></body></html>"
+    return HttpResponse(html)
