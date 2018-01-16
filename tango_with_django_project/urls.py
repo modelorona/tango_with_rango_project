e6ba99af41862c28a8052b3bafaff34da4d4df3e
Anguel <angel.hristozov@gmail.com>
2018-01-16 23:22:26 +0000

    fixed about page route

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -19,8 +19,8 @@ from django.conf.urls import include
 from rango import views
 
 urlpatterns = [
-    url(r'$', views.index, name='index'),
-    url(r'rango/', include('rango.urls')),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    # url(r'^admin/', admin.site.urls),
 ]
 
