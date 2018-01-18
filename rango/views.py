2f14172010684bdb0d8e4c46405f197d4b0c1fa3
Anguel <angel.hristozov@gmail.com>
2018-01-18 18:53:33 +0000

    added rango image and change settings to include template directory as well as static

--- rango/views.py
+++ rango/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    html = "<html><head></head><body>Hi there. <a href='/rango/about/'>About page</a></body></html>"
-    return HttpResponse(html)
+    context = {
+        'boldmessage': 'Crunchy creamy cupcake yo'
+    }
+    return render(request, 'rango/index.html', context=context)
 
 
 def about(request):
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Templates path
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
+# Static files path
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +61,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -100,6 +106,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATICFILES_DIRS = [STATIC_DIR]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -113,9 +121,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
